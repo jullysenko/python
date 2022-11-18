1cf9f07130a1f23547493cc61b9d7274f8e5b31e
Julia <jskredova@yandex.ru>
2022-11-18 23:38:15 +0300

    add seminar 1

---
+++ seminar_1/task_check_thuth.py
@@ -0,0 +1,15 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+
+def check_thuth(x, y, z):
+    print(not (x or y or z) == (not x and not y and not z))
+
+
+check_thuth(False, False, False)
+check_thuth(True, False, False)
+check_thuth(False, True, False)
+check_thuth(False, False, True)
+check_thuth(True, True, False)
+check_thuth(False, True, True)
+check_thuth(True, False, True)
+check_thuth(True, True, True)
