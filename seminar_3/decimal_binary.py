586ce8defd0ba553cfd50c4221b6b254febf471b
Julia <jskredova@yandex.ru>
2022-12-09 09:47:36 +0300

    add seminar 3

---
+++ seminar_3/decimal_binary.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+
+number = int(input('Введите число: '))
+temp = number
+res_str = ''
+while number // 2 > 0:
+    res_str += str(number % 2)
+    number = number // 2
+    if number == 1:
+        res_str += '1'
+
+print(f'{temp} = {res_str[::-1]}')
